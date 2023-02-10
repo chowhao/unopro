95a014fd4dd3bcb65b7ae2643bfaf2c1c982d920
chowhao <91246671+2076625923@users.noreply.github.com>
2023-02-10 15:05:03 +0800

    Update unpro.c

--- unpro.c
+++ unpro.c
@@ -1,5 +1,5 @@
 /*###########################################################
-### File Name: unpro.c
+### File Name: unopro.c
 ### Author: Chowhao
 ### Mail: 19211448@bjtu.edu.cn
 ### Created Time: 2023-01-15
@@ -9,7 +9,6 @@
 #include <string.h>
 #include <fcntl.h>
 #include <termios.h>
-#include <stdlib.h>
 
 #define SIZE 0x8000
 
@@ -31,7 +30,7 @@ int main(int argc, char *argv[])
   char *port = argv[2];
   char *baud = argv[1];
   
-
+  /* specify baudrate */
   long int speed = strtol(baud, NULL, 0);	
   if(!(speed == 9600 || speed == 19200 || speed == 38400))
   {
@@ -39,7 +38,8 @@ int main(int argc, char *argv[])
     printf("Optional BaudRate is 9600, 19200 and 38400 !\n");
     return 1;
   }
-  
+
+  /* try to connect */
   int fd = plug(speed, port);
 
   if(fd == -1)
@@ -48,101 +48,84 @@ int main(int argc, char *argv[])
     return 1;
   }
   /* success open port */
-  else  printf("Success open %s, BaudRate is %ld !\n", port, speed);
-
-
-  /* command */
-  char cmd[32];
-  char *ptr = cmd;
-  char *optv[] = { "help", "dump", "load", "erase", "write", "demo", "clear", "list"};
-
-  /* argument */
-  char arg[16][16];
+  else  
+  {
+    printf("Success open %s, BaudRate is %ld !\n", port, speed);
+    printf("Please type 'help' for usage\n");
+  }
   
-  /* others */
-  char *ps1 = ">>> ";
-  char *exit = "quit";
+  /* command line and argument */
+  char cli[32];
+  char *ptr = cli;
+  char arg[16][16];
+  char *cmdv[] = { "dump", "write", "erase", "load", "help", "exit"};
 
   /* get command and argument */ 
-  printf("Please type 'help' for usage\n");
+  char *ps1 = ">>> ";
   printf("%s", ps1);
-  gets(cmd); 
+  gets(cli); 
 
 
-  /* continue until quit command */
-  while(strcmp(cmd, exit) != 0)
+  /* quit command */
+  while(strcmp(cli, cmdv[5]) != 0)
   { 
-    /* get cmd char num */
-    int num = strchr(cmd, '\0')-cmd;
+    /* input char num */
+    int num = strchr(cli, '\0')-cli;
 
-    /* nothing input*/ 
+    /* nothing */ 
     if(num == 0)
     {
       printf
-      (
-        "\033[1;33mNothing input at all !\033[0m\n"
-        "Please type 'help' or 'demo' for help !\n"
+      ( "\033[1;33mNothing input at all !\033[0m\n"
+        "Please type 'help' for usage !\n"
       );
     }
     else
-    { /* something input */
+    { 
+      /* get command and argument */
+      int cmdc = 0;
       char seps[] = " ";
-      int index = 0;
 
-      /* split cmd to get command and argument */
-      char *cli = strtok_r(cmd, seps, &ptr);
-      while(cli != NULL)
+      char *tmp = strtok_r(cli, seps, &ptr);
+      while(tmp != NULL)
       {
-        strcpy(arg[index], cli);
-        index++;
-        cli = strtok_r(NULL, seps, &ptr);
+        strcpy(arg[cmdc], tmp);
+        cmdc++;
+        tmp = strtok_r(NULL, seps, &ptr);
       }
-
-      if(index != 0)
+      
+      if(cmdc != 0)
       { 
-        /* get cmd flag */
-        int flag = -1;
-        flag = getcmd(optv, arg[0]);
+        /* get cmd index */
+        int index = -1;
+        index = getcmd(cmdv, arg[0]);
 
-        if(flag != -1)
+        if(index != -1)
         { 
-          if(flag == 0)
+          if(index == 4)
           { /* help command */
-            if(index == 1) usage();
+            if(cmdc == 1) usage();
             else printf("Do you mean 'help' ?\n");
-          }
-          else if(flag == 5)
-          {
-            if(index == 1) warn();
-            else printf("Do you mean 'demo' ?\n");
-          }
-          else if(flag == 6)
-          {
-            if(index == 1) system("clear");
-            else printf("Do you mean 'clear' ?\n");
-          }
-          else if(flag == 7)
-          {
-            if(index == 1) system("ls");
-            else printf("Do you mean 'list' ?\n");
           }
           else
           {
-            if(index == 3)
+            if(cmdc == 3)
             { 
-              /* handle operation */
-              if((strchr(arg[1], '\0')-arg[1]) != 4)
-              { /* address error */
-                printf("\033[1;33mAddress must be two bytes without prefix !\033[0m\n");
-                printf("Your address: %s !\033[0m\n", arg[1]);
-                printf("Type 'demo' for usage examples\n");
+              /* address must like 01f0 */
+              int addr = hex2int(arg[1]);
+              int orgc = strchr(arg[1], '\0')-arg[1];
+              
+              if(orgc != 4 || addr == -1)
+              { 
+                printf("\033[1;33mAddress should be Hex without prefix, like 0100 !\033[0m\n");
+                printf("However, your address: %s !\033[0m\n", arg[1]);
               }
-              else operation(fd, flag, arg);
+              else process(fd, index, arg);
             }
             else
-            { /* argument error */
-              printf("Do you mean '%s' ?\n", optv[flag]);
-              printf("Type 'demo' for usage examples\n");
+            { /* give example usage */
+              printf("Do you mean '%s' ?\n", cmdv[index]);
+              example(index);
             }
 
           }
@@ -151,23 +134,23 @@ int main(int argc, char *argv[])
         else
         { /* unknown command */
           printf("\033[1;33m%s ?\033[0m\n", arg[0]);
-          printf("Please type 'help' for usage\n");
+          printf("Please type 'help' for usage !\n");
         }
 
-        flag = -1;
+        index = -1;
       }
 
-      index = 0;
+      cmdc = 0;
 
     }
 
-    /* get command line again */      
+    /* get command ang argument */      
     printf("%s", ps1);
-    gets(cmd); 
+    gets(cli); 
 
   }
   
-  /* close */
+  /* close port */
   close(fd);
   return 0;
 
@@ -177,12 +160,12 @@ int main(int argc, char *argv[])
 /* initial serial port */
 int plug(long int baud, char *port)
 {
-  /* get port fd */
   int fd = open(port, O_RDWR);
 
+  /* success open port */
   if(fd != -1)
   { 
-    /* get port settings */
+    /* get current port settings */
     struct termios ttyopt;
     tcgetattr(fd, &ttyopt);
 
@@ -203,75 +186,140 @@ int plug(long int baud, char *port)
       if(baud == speed[j]) cfsetispeed(&ttyopt, rate[j]);
     }
 
-    /* commit port settings */
+    /* commit settings after change*/
     tcsetattr(fd, TCSANOW, &ttyopt);
   }
+
   return fd;
 }
 
 
-/* convert hex to int */
-int hex2int(char *hex)
+/* get command index */
+int getcmd(char *cmdv[], char *cmd)
 {
-  int sum = 0;
-  int num = strchr(hex, '\0')-hex;
-	for(int j=0; j<num; j++)
-	{
-    if (hex[j] >= '0' && hex[j] <= '9') sum = 0;
-    else if (hex[j] >= 'a' && hex[j] <= 'f') sum = 0;
-    else if (hex[j] >= 'A' && hex[j] <= 'F') sum = 0; 
-    else return -1;
-	}
-	
-  char str[] = "0xffff";
-  for(int j=0; j<4; j++) str[j+2] = hex[j];
-  sum = strtol(str, NULL, 0);	
-	return sum;
-}
 
-
-/* get option from input */
-int getcmd(char *opts[], char *opt)
-{
-  for(int i=0; i<8; i++)
+  for(int i=0; i<5; i++)
   {
-    /* return operation index */
-    if(strcmp(opt, opts[i]) == 0) return i;
+    /* return command index */
+    if(strcmp(cmd, cmdv[i]) == 0) return i;
   }
-  /* unknown operation */
+  /* unknown command */
   return -1;
 }
 
 
-/* handle operation */
-int operation(int fd, int flag, char arg[16][16])
+/* example usage */
+void example(int index)
+{
+  printf("Examples: ");
+  if(index == 0) printf("dump 0100 02ff    Read 0x02ff bytes from 0x0100\n");
+  else if(index == 1) printf("write 01f0 2ff635 Write 2fh,f6h,35h from 0x01f0\n");
+  else if(index == 2) printf("erase 02f0 0100   Erase 0x0100 bytes from 0x02f0\n");
+  else /* index == 3*/printf("load 01ff a.bin   Write file a.bin  from 0x01ff\n");
+}
+
+
+/* print help page */
+void usage(void)
+{
+  printf
+  ( "dump   [org]  [size]     Read some bytes from address\n"
+    "write  [org]  [data]     Write some bytes to address\n"  
+    "erase  [org]  [size]     Erase some bytes from address\n"
+    "load   [org]  [file]     Write binary file to address\n"  
+    "help                     Display this help page !\n"
+    "exit                     Exit from this command tool\n"
+  );
+}
+
+
+/* process command */
+int process(int fd, int index, char arg[16][16])
 {
   /* address and num */
   int org = hex2int(arg[1]);
-  int num = hex2int(arg[2]);
-  int all = strchr(arg[2], '\0')-arg[2];
+  int data = hex2int(arg[2]);
+  int arg2c = strchr(arg[2], '\0')-arg[2];
 
-  if(org == -1 || (flag != 2 && num == -1))
+  if(index != 3 && data == -1)
   {
-    printf("\033[1;33mAddress and size should be hex without prefix !\033[0m\n");
-    printf("Please specify address and bytes again !\n");
+    printf("\033[1;33mSize and Data should be Hex without prefix, like 01f0, 01f3 !\033[0m\n");
     return 1;
   }
   
   /* dump command */
-  if(flag == 1)
+  if(index == 0)
   {
-    int size = (num%16 != 0 ? (num/16 + 1)*16 : num);
-    if(all == 4 && size != 0 && size+org < SIZE+1) dump(fd, arg[1], arg[2]);
+    /* read 16 times bytes once */
+    int size = (data%16 != 0 ? (data/16 + 1)*16 : data);
+    if(arg2c == 4 && size != 0 && size+org <= SIZE) dump(fd, arg[1], arg[2]);
     else
     {
-      if(size+org > SIZE) printf("\033[1;33m%sh + %sh > 8000h !\033[0m\n", arg[1], arg[2]);
-      else printf("\033[1;33mAddress:%s, Size:%s !\033[0m\n", arg[1], arg[2]);
-      printf("Please specify address and size again !\n");
+      if(size+org > SIZE)
+      {
+        printf("\033[1;33m%sh + %sh > 8000h !\033[0m\n", arg[1], arg[2]);
+        printf("Addresss is 0x0000 ~ 0x7fff, specify your argment again!\n");
+      }
+      if(arg2c != 4 || size == 0)
+      {
+        printf("\033[1;33mSize should be Hex without prefix and more than 0000, like 0100, 01f0 !\033[0m\n");
+        printf("\033[1;33mHowever, your size: %s !\033[0m\n", arg[2]);
+      }
+    }
+  }
+  /* write command */
+  else if(index == 1) 
+  {
+    int num = arg2c/2;
+
+    if(num%2 == 0 && num != 0 && num+org <= SIZE)
+    {
+      mywrite(fd, arg[1], arg[2]);
+
+      /* sleep for wrtite */
+      for(int cur=1; cur<=num; cur++)
+      {
+      usleep(20*1000);
+      printf("Total: %d Bytes, Written: %d Bytes\r", num, cur);
+		  fflush(stdout);
+      }
+      printf("\n");
+    }
+    else
+    {
+      if(num+org > SIZE)
+      {
+        printf("\033[1;33m%sh + %dd > 8000h !\033[0m\n", arg[1], num);
+        printf("Addresss is 0x0000 ~ 0x7fff, specify your argument again!\n");
+      }
+
+      if(num%2 != 0 || num == 0)
+      {
+        printf("\033[1;33mDate should be Even and  Hex without prefix, like 010001f0 !\033[0m\n");
+        printf("\033[1;33mHowever, your data:%s !\033[0m\n", arg[2]);
+      }
+    }
+  }
+  /* erase command */
+  else if(index == 2)
+  {
+    if(arg2c == 4 && data+org <= SIZE) erase(fd, arg[1], arg[2]);
+    else
+    {
+      if(data+org > SIZE)
+      {
+        printf("\033[1;33m%sh + %sh > 8000h !\033[0m\n", arg[1], arg[2]);
+        printf("Addresss is 0x0000 ~ 0x7fff, specify your argument again!\n");
+      }
+      if(arg2c != 4)
+      {
+        printf("\033[1;33mSize should be Hex without prefix, like 01f0 !\033[0m\n");
+        printf("\033[1;33mHowever, your size:%s !\033[0m\n", arg[2]);
+      }
     }
   }
   /* load command */
-  else if(flag == 2) 
+  else if(index == 3) 
   {
     FILE *file = fopen(arg[2], "rb");
     if(!file) printf("\033[1;33mFail to open %s !\033[0m\n", arg[2]);
@@ -282,96 +330,54 @@ int operation(int fd, int flag, char arg[16][16])
 
       if(len+org > SIZE)
       {
-        printf("\033[1;33m%sh + %dd > 8000h !\033[0m\n", arg[1], len);
-        printf("Please specify address and file again !\n");
+        printf("\033[1;33m%sh + %04x > 8000h !\033[0m\n", arg[1], len);
+         printf("Addresss is 0x0000 ~ 0x7fff, specify your argument again!\n");
       }
       else load(fd, arg[1], file);
       /* close file */
       fclose(file);
     }
-  }
-  /* erase command */
-  else if(flag == 3)
-  {
-    if(all == 4 && num != 0 && num+org < SIZE+1) erase(fd, arg[1], arg[2]);
-    else
-    {
-      if(num+org > SIZE) printf("\033[1;33m%sh + %sh > 8000h !\033[0m\n", arg[1], arg[2]);
-      else printf("\033[1;33mAddress:%s, Size:%s !\033[0m\n", arg[1], arg[2]);
-      printf("Please specify address and size again !\n");
-    }
-  }
-  /* write command */
-  else if(flag == 4) 
-  {
-    int half = all/2;
-
-    if(all%2 == 0 && half != 0 && half+org < SIZE+1)
-    {
-      mywrite(fd, arg[1], arg[2]);
-
-      /* sleep for wrtite */
-      for(int current=0; current<half; current++)
-      {
-      usleep(20*1000);
-      printf("Total: %d Bytes, Burned: %d Bytes\r", half, current+1);
-		  fflush(stdout);
-      }
-      printf("\n");
-    }
-    else
-    {
-      if(half+org > SIZE) printf("\033[1;33m%sh + %dd > 8000h !\033[0m\n", arg[1], half);
-      else printf("\033[1;33mAddress:%s, Data:%s !\033[0m\n", arg[1], arg[2]);
-      if(all%2 != 0) printf("The data should be Even, not Odd!\n");
-      printf("Please specify address and size again !\n");
-    }
-  }
-  /* argument error */     
+  } 
   else
-  {
+  { /* argument error */    
     printf("\033[1;33mArgument specify error !\033[0m\n");
-    warn(); 
+    printf("Please type 'help' for usage !\n");
   }
   return 0;          
 }
 
 
-/* operation right but argument error */
-void warn(void)
+/* convert hex to int */
+/* input:07ff, output:2047 */
+int hex2int(char *hex)
 {
-  printf(
-    "Examples:\n"
-    "dump 0100 02ff    Read 0x02ff bytes from 0x0100\n"
-    "load 01ff a.bin   Write file a.bin  from 0x01ff\n"
-    "write 01f0 2ff635 Write 2fh,f6h,35h from 0x01f0\n"
-    "erase 02f0 0100   Erase 0x0100 bytes from 0x02f0\n");
+  int sum = 0;
+  int num = strchr(hex, '\0')-hex;
+
+	for(int j=0; j<num; j++)
+	{
+    if (hex[j] >= '0' && hex[j] <= '9') sum = 0;
+    else if (hex[j] >= 'a' && hex[j] <= 'f') sum = 0;
+    else if (hex[j] >= 'A' && hex[j] <= 'F') sum = 0; 
+    /* some wrong char */
+    else return -1;
+	}
+	
+  char str[] = "0xffff";
+  for(int j=0; j<4; j++) str[j+2] = hex[j];
+  sum = strtol(str, NULL, 0);	
+
+	return sum;
 }
 
 
-/* print help page */
-void usage(void)
-{
-  printf
-  (
-    "dump   [org]  [size]     Read some bytes from address\n"
-    "load   [org]  [file]     Write binary file to address\n"  
-    "write  [org]  [data]     Write some bytes to address\n"  
-    "erase  [org]  [size]     Erase some bytes from address\n"
-    "clear                    Clear out the screen !\n"
-    "list                     List all the files in pwd !\n"
-    "help                     Display this help page !\n"
-    "demo                     Display some usage examples\n"
-    "quit                     Exit from this command tool\n"
-  );
-}
-
-
-/* read some bytes from device */
+/* read some bytes */
+/* org:0100, size:0030 */
 void dump(int fd, char *org, char *size)
 {
   char msg[128];
-  char buf[256];
+  char buf[256] = {'\0'};
+
   /* specify read operation */
   msg[0] = '0';
   msg[1] = '0';
@@ -389,14 +395,14 @@ void dump(int fd, char *org, char *size)
 
   /* read some bytes */
   int num = read(fd, buf, sizeof(buf));
-  int flag = getout(buf);
+  int out = getout(buf);
 
-  while(num > 0 && flag == 0)
+  while(num > 0 && out == 0)
   {
     printf("%s", buf);
     for(int i=0; i<256; i++) buf[i] = '\0';
     num = read(fd, buf, sizeof(buf));
-    flag = getout(buf);
+    out = getout(buf);
   }
 
   printf("%s", buf);
@@ -409,7 +415,7 @@ void dump(int fd, char *org, char *size)
 }
 
 
-/* exit from loop after read 'Done! */
+/* exit from dump loop */
 int getout(char *str)
 {
 	for(int j=0; j<4; j++)
@@ -420,10 +426,11 @@ int getout(char *str)
 }
 
 
-/* erase some bytes in device */
+/* erase some bytes */
 void erase(int fd, char *org, char *size)
 {
   char msg[128];
+
   /* specify erase operation */
   msg[0] = '0';
   msg[1] = 'e';
@@ -439,13 +446,13 @@ void erase(int fd, char *org, char *size)
   /* send message to device */
   write(fd, msg, 11);
 
-  /* sleep for wrtite */
+  /* sleep for erase */
   int sum = hex2int(size);
 
-  for(int current=0; current<sum; current++)
+  for(int cur=0; cur<sum; cur++)
   {
     usleep(20*1000);
-    printf("Total: %d Bytes, Erased: %d Bytes\r", sum, current+1);
+    printf("Total: %d Bytes, Erased: %d Bytes\r", sum, cur+1);
 		fflush(stdout);
   }
   printf("\n");
@@ -453,13 +460,16 @@ void erase(int fd, char *org, char *size)
 }
 
 
-/* write some bytes to device */
+/* write some bytes */
+/* data: 2345 --> 0x23h, 0x45h */
 void mywrite(int fd, char *org, char *data)
 {
   char msg[128];
+
   /* specify write operation */
   msg[0] = '0';
   msg[1] = '1';
+  /* num = 2 * bytes */
   int num = strchr(data, '\0')-data;
   msg[6+num] = '\n';
   
@@ -481,40 +491,39 @@ void mywrite(int fd, char *org, char *data)
 }
 
 
-/* write binary file to device */
+/* write binary file */
 void load(int fd, char *org, FILE *file)
 {
   /* get file size */
   fseek(file, SEEK_SET, SEEK_END);
   int len  = ftell(file);
-
-	/* address and data */
-  int ch;
-  int sum = 0;
-  char tmp[4];
-  char data[96];
-
-  /* integer address */
-  int address = hex2int(org);
-
   /* seek file beginning */
 	fseek(file, SEEK_SET, SEEK_SET );
+
+	/* address and data */
+  int bin;
+  int sum = 0;
+  char tmp[4];
+  char data[96] = {'\0'};
+  int addr0 = hex2int(org);
   
-  for(int j=0; j<len; j += 48)
+  /* write 48 bytes once */
+  for(int addr=0; addr<len; addr += 48)
 	{
 		for(int k=0; k<48; k++)
 		{
-      if((ch = fgetc(file)) != EOF)
+      if((bin = fgetc(file)) != EOF)
 			{
-				sprintf(tmp, "%02x", ch);
+				sprintf(tmp, "%02x", bin);
 				data[2*k] = tmp[0];
 				data[2*k+1] = tmp[1];
+        /* real bytes num */
         sum += 1;
 			}
 		}
 
-		/* specify address */
-		sprintf(org, "%04x", j + address);
+		/* current address and org */
+		sprintf(org, "%04x", addr + addr0);
 
 		/* write some bytes */
     mywrite(fd, org, data);
@@ -526,10 +535,10 @@ void load(int fd, char *org, FILE *file)
 		}
 
     /* sleep for wrtite */
-    for(int current=0; current<sum; current++)
+    for(int cur=0; cur<=sum; cur++)
     {
       usleep(20*1000);
-      printf("Total: %d Bytes, Burned: %d Bytes\r", len, current+j+1);
+      printf("Total: %d Bytes, Burned: %d Bytes\r", len, cur+addr);
 		  fflush(stdout);
     }
     sum = 0;
